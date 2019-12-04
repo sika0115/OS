887fe6aa3dae391dea04554f469f0eb7dd908253
sika0115 <nh3988@gmail.com>
2019-12-04 12:06:21 +0900

    StaticLibrary in

---
+++ StaticLibrary/sample_main.c
@@ -0,0 +1,9 @@
+//sample_main.c
+#include <stdio.h>
+int static_function();
+
+int main() {
+    printf("Hello World\n");
+    static_function();
+    return 0;
+}
---
+++ StaticLibrary/static_library.c
@@ -0,0 +1,7 @@
+//static_library.c
+#include <stdio.h>
+
+int static_function() {
+    printf("Static Library\n");
+    return 0;
+}
